227ac96fcaa3732279b54a89b80829361cbb5374
Jennifer Ponder <jponder2000@yahoo.com>
2018-03-03 19:40:05 -0500

    wipupdate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,11 @@
 // Variables for the game
 // ===============================================
 
-//Wins
-var wins = 0
-
-//Losses
-var losses = 0
+var wins = 0;  //Wins
+var losses = 0; //Losses
 
 //Guesses palyer has left
-var guessesLeft = 10;
+var guessesLeft = 3;
 
 //Player guess the correct letter
 var playerGuessC = [];
@@ -17,7 +14,7 @@ var playerGuessC = [];
 var playerGuessI = [];
 
 //Random word chosen by the program
-var randomWord = [];
+var randomWord = '';
 
 //Array of words for the game...the answers
 var fashionWords = ['peplum','lace','leather','houndstooth','midi','shortalls','pumps','oxfords'];
@@ -42,190 +39,231 @@ var myMusic
 //---GAME PLAY STARTS NOW--
 //I have to set the words up so the for loop can loop through the array and set the random code to place a random word//
 
-// Generate Random Words and store them in a variable....an Array?
-// Game start
-// Make underscores
 //Determine if player guesses for the letters are correct
 //Determine if player guesses for the letteres are incorrect
 
+// Generate Random Words and store them in a variable....an Array?
+// Game start
+// Make underscores
 
 // var answerArray = [];
 // for (var i = 0; i < fashionWords.length i++) {
-//   answerArray[i] = "_";
-// }
-
+//   answerArray[
 
 
 //================================================================================
-//FUNCTIONS
+//FUNCTIONS}
 
-function playGameRandomWord() {
-
- var randomWord = fashionWords[Math.floor(Math.random()) * fashionWords.length];
-  return; randomWord;
-
-for (var i = 0; i < randomWord.length; i++) {
-
-     
-     underScore.push(' _ ');
-
-    }
-     
-     fashionWords.push(randomWord[i]);
-
+function getWord(){
+  randomWord = fashionWords[Math.floor(Math.random() * fashionWords.length)];
 }
 
-//Or if underScore does not show up try this below.....
-//To get underScores to show up 
+function underScores(){
+  underScore = [];
+  for (var i = 0; i < randomWord.length; i++) {   
+     underScore.push('_');
+  }   
+}
+function getLetter(){
+  document.onkeyup = function(event){
+    console.log(event.key);
+    checkWord(event.key);
+  } 
+}
+function checkWord(ltr){
+ if (randomWord.indexOf(ltr) === -1){
+      badGuess();
+  } else { 
+      goodGuess(ltr);
+  }
+}
+function badGuess(){
+    guessesLeft--;
+    isGameOver();
+}
+function goodGuess(ltr){
+  underScore[randomWord.indexOf(ltr)] = ltr;
+  console.log(underScore);
+  isGameOver();
+}
+function isGameOver(){
+  if (guessesLeft === 0){
+    alert('You Lose!');
+  } else if (underScore.indexOf('_') === -1){
+    alert('You Win!');
+  }
 
- document.getElementbyID('word').textContent = underScores.join(" ");
+}
+function playGame(){
+  guessesLeft = 3;
+  getWord();
+  underScores();
+  console.log(randomWord,underScore);
+  getLetter();
+}
+playGame();
+
+// function playGameRandomWord() {
+
+  
+
+  
+//   fashionWords.push(randomWord[i]);i] = "_";
+// // }
 
 
-playGameRandomWord();
+
+// //Or if underScore does not show up try this below.....
+// //To get underScores to show up 
+
+//  document.getElementbyID('word').textContent = underScores.join(" ");
 
 
-document.onkeyup = function(event){
- var playerGuess = event.key;
+// playGameRandomWord();
 
-  console.log(playerGuess.toUpperCase())
-  if (randomWord.indexOf(playerGuess) > -1){
-//Checks to see if the letter exists inside of the word
+
+// document.onkeyup = function(event){
+//  var playerGuess = event.key;
+
+//   console.log(playerGuess.toUpperCase())
+//   if (randomWord.indexOf(playerGuess) > -1){
+// //Checks to see if the letter exists inside of the word
    
-      if (randomWord[i] == playerGuess)
-   {
-       underScores[i] = playerGuess;
-       console.log(underScores); 
+//       if (randomWord[i] == playerGuess)
+//    {
+//        underScores[i] = playerGuess;
+//        console.log(underScores); 
 
-   } 
+//    } 
 
-}
+// }
 
-else { 
-  playerGuessI.push(playerGuess);
-guessesLeft--;
+// else { 
+//   playerGuessI.push(playerGuess);
+// guessesLeft--;
 
-   }
+//    }
 
-}
+// }
 
-//=======================================================================================================
+// //=======================================================================================================
      
-// At Game Play Music Starts
+// // At Game Play Music Starts
 
-function playStartMusic () {
+// function playStartMusic () {
 
-  myMusic = new sound ("deepHouse1.wav");
-  myMusic.play ();
-  myGameStart.start ();
-}
+//   myMusic = new sound ("deepHouse1.wav");
+//   myMusic.play ();
+//   myGameStart.start ();
+// }
 
-playStartMusic();
-//======================================================================================================
-
-
-//Check to see if letters are correct-player's guesses/ and determines guesses left
-guessesLeft = 10;
-
-function evaluateGuessesLeft() {
-
- if(randomWord.indexOf(playerGuess) > -1)
-{
-
-  if (randomWord[0] == playerGuess)
-   {
-    underScores[i] = playerGuesss
-
-   }
-}
- else{
-
-      playerGuessI.push(playerGuess)
-       // underScores[i] = playerGuesss
-}
-
-evaluateGuessesLeft();
+// playStartMusic();
+// //======================================================================================================
 
 
-document.getElementbyID('guessesLeft').innerHTML = guessesLeft;
+// //Check to see if letters are correct-player's guesses/ and determines guesses left
+// guessesLeft = 10;
 
-//To look at guesses left....or look at doing it this way
- // document.getElementByID('guessesLeft').textContent = guessesLeft;
+// function evaluateGuessesLeft() {
 
+//  if(randomWord.indexOf(playerGuess) > -1)
+// {
 
- //reset guess
- incorrectLetter = []
- guessesLeft = 10;
+//   if (randomWord[0] == playerGuess)
+//    {
+//     underScores[i] = playerGuesss
 
+//    }
+// }
+//  else{
 
-// If the letter is wrong do nothing until user gets letter correct with guesses left
+//       playerGuessI.push(playerGuess)
+//        // underScores[i] = playerGuesss
+// }
 
-
-
-//If the user guesses the correct letter show letter
-
-
-if (event.key == true) {
-
-  return playerGuessC;
-}
-
-//IMAGES To make images not appear, but appear when user guesses correct and guesses incorrect
-//============================================================================================
-function showImage() {
-    var img = document.getElementById('photo-size');
-    img.style.visibility = 'visible';
-}
-
-//===========================================================================================
-
-// If user guesses correct word user score increases by 1 and indicates a win that increases win attempts
-
-function checkWin() {
-    for (var i = 0; i < fashionWords.length; i++) {
-        if (randomWordArray[i] == fashionWords.length) {
-           console.log(true);
-            return true;
-        }
-    }
-    game.playerGuess += 1;
-
-    return true;
-    winCount++;
-}
-
-checkWin();
-
-function alertWinner(){
-if (playerGuessC == randomWord[0]){
-  alert('You Are A Winner');
-  location.reload();
-
-  }
-
-}
-alertWinner();
+// evaluateGuessesLeft();
 
 
-//If user guesses incorrect word user score decreases and indicates a loss that increases on loss attempts 
+// document.getElementbyID('guessesLeft').innerHTML = guessesLeft;
 
-function checkLoss() {
-   for (var i = 0; i < fashionWords.length; i++) {
-        if (randomWordArray[i] != fashionWords.length) {
-            return false;
-        }
-    }
-    if (playerGuess == 0) {
-        playerGuess += 1;
-        return true;
-    }
-    return false;
-    lossCount++;
-}
+// //To look at guesses left....or look at doing it this way
+//  // document.getElementByID('guessesLeft').textContent = guessesLeft;
 
-function alertLoss(){
- if (guessesLeft < 1){
-    alert('You Lost, Time To Try Again!');
-    location.reload();
-  }
 
-alertLoss();
+//  //reset guess
+//  incorrectLetter = []
+//  guessesLeft = 10;
+
+
+// // If the letter is wrong do nothing until user gets letter correct with guesses left
+
+
+
+// //If the user guesses the correct letter show letter
+
+
+// if (event.key == true) {
+
+//   return playerGuessC;
+// }
+
+// //IMAGES To make images not appear, but appear when user guesses correct and guesses incorrect
+// //============================================================================================
+// function showImage() {
+//     var img = document.getElementById('photo-size');
+//     img.style.visibility = 'visible';
+// }
+
+// //===========================================================================================
+
+// // If user guesses correct word user score increases by 1 and indicates a win that increases win attempts
+
+// function checkWin() {
+//     for (var i = 0; i < fashionWords.length; i++) {
+//         if (randomWordArray[i] == fashionWords.length) {
+//            console.log(true);
+//             return true;
+//         }
+//     }
+//     game.playerGuess += 1;
+
+//     return true;
+//     winCount++;
+// }
+
+// checkWin();
+
+// function alertWinner(){
+// if (playerGuessC == randomWord[0]){
+//   alert('You Are A Winner');
+//   location.reload();
+
+//   }
+
+// }
+// alertWinner();
+
+
+// //If user guesses incorrect word user score decreases and indicates a loss that increases on loss attempts 
+
+// function checkLoss() {
+//    for (var i = 0; i < fashionWords.length; i++) {
+//         if (randomWordArray[i] != fashionWords.length) {
+//             return false;
+//         }
+//     }
+//     if (playerGuess == 0) {
+//         playerGuess += 1;
+//         return true;
+//     }
+//     return false;
+//     lossCount++;
+// }
+
+// function alertLoss(){
+//  if (guessesLeft < 1){
+//     alert('You Lost, Time To Try Again!');
+//     location.reload();
+//   }
+
+// alertLoss();
